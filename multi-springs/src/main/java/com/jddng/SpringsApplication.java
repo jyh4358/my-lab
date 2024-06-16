cf31d57436cf8064ec8b2be8b0aae08705f5ed46
yoonho <jyh4358@gmail.com>
2024-06-16 18:33:43 +0900

    init

---
+++ multi-springs/src/main/java/com/jddng/SpringsApplication.java
@@ -0,0 +1,11 @@
+package com.jddng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsApplication.class, args);
+    }
+}
